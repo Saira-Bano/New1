83261ce9cb410a7e196192cf894198485227ab08
Fazeel <Fazeel@192.168.1.3>
2016-11-04 21:44:19 +0500

    new

---
+++ new1/src/new1/New1.java
@@ -0,0 +1,17 @@
+
+
+package new1;
+
+/**
+ *
+ * @author Fazeel
+ */
+public class New1 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+    
+}
